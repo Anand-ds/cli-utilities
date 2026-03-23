5b6e97129e6132b7c856e67a7723a984a2c8e069
Anand Sundaresan <60470539+Anand-ds@users.noreply.github.com>
2026-03-23 17:12:35 +0000

    Update main.py
    App Entry Point

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+from tools.calculator import calculator
+from tools.todo import todo_menu
 
+def main():
+  while True:
+      print ("\n=== CLI-Utilities ===")
+      print ("1.Calculator")
+      print ("2.To-Do List")
+      print ("3.Exit")
+
+      choice = Input("Choose an option")
+
+      if choice == "1":
+        calculator()
+      elif choice == "2":
+        todo_menu()
+elif choice == "3":
+  print ("Goodbye!")
+  break
+else:
+  print ("Invalid choice. try again.")
+
+if __name__ == "__main__":
+  main()
+  
